1360773db5ce6d8f611a646f6409b7958cacb481
Dennis Hawran <56675253+AtmoGD@users.noreply.github.com>
2023-05-24 15:32:57 +0200

    Added config.json & First try to fetch it

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/tags", (req, res) => RequestTags(req, res));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function RequestTags(req, res) {
+  config = fetch('./config.json');
+  print(config);
+}
+
+function RequestConcept(req, res) {
+
+}
 
 const html = `
 <!DOCTYPE html>
